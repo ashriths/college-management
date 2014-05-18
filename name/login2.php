795a94c3116c07fbdc3d71612f9f0f30c6de3b31
ashishrawat93 <puneashish@yahoo.co.in>
2014-05-18 08:53:09 +0530

    latest state of the project

--- name/login2.php
+++ name/login2.php
@@ -1,3 +1,31 @@
+<?php
+require_once('redirect.php');
+require_once('php/function.php');
+if(isset($_SESSION['id'])){
+  //redirect to homepage
+  #Redirect::redirectTo('home.php');
+
+  header("Location: home.php");
+}
+if(isset($_POST['usn'])){
+  $result = $user->validateUser($_POST['usn'],$_POST['password']);
+  if($result>0){
+    
+    $session->createSession($result);
+    header("Location: home.php");
+  }
+  else
+  {
+    $session->createSessionTemp($_POST['usn']);
+    #created a temp session to pass usn value
+    
+    header("Location: login.php");
+  }
+  
+}
+
+?>
+
 <html lang="en"><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,11 +36,12 @@
 
     <title>Cover Template for Bootstrap</title>
 
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="bootstrap/css/signin.css" rel="stylesheet">
+    
     <link href="bootstrap/css/cover.css" rel="stylesheet">
 
-    
   <style id="holderjs-style" type="text/css"></style></head>
 
   <body>
@@ -27,8 +56,8 @@
             <div class="inner">
               <h3 class="masthead-brand" style="font-weight:normal;">BMSCE</h3>
               <ul class="nav masthead-nav">
-                <li class="active"><a href="cover.php">Home</a></li>
-                <li><a href="login2.php">Login</a></li>
+                <li ><a href="cover.php">Home</a></li>
+                <li class="active"><a href="login2.php">Login</a></li>
                 <li><a href="contact.php">Contact</a></li>
               </ul>
             </div>
@@ -36,14 +65,19 @@
 
           <div class="inner cover">
             <h1 class="cover-heading">BMSAPP<p style="font-size:15pt;font-weight:normal;"> Initialize the Awesomeness</p></h1>
-            
-            <p class="lead">BMSAPP is a cool way to view college marks and attendance. Redundancy and Effort eradicated. The 'lazy ones' will love it. </p>
-            <p class="lead">
-              <a href="learn.php" class="btn btn-lg btn-default">Learn more</a>
-            </p>
-          </div>
+            </div>
+            <form style="" class="form-signin" role="form" action="login2.php" method="post">
+            <h2 class="form-signin-heading">Please sign in</h2>
+                  <input type="text" class="form-control" name="usn" placeholder="USN" required autofocus>
+                  <input type="password" class="form-control" name="password" placeholder="Password" required>
+                  <label class="checkbox">
+                    <input type="checkbox" value="remember-me"> Remember me
+                  </label>
+                 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+          
 
-          <div class="mastfoot">
+            <div class="mastfoot">
             
             <div class="inner">
               <p>&copy <a>ashrith</a>.alta.<a>anuj</a>.ashish.<a>jags</a>.divya</p>
